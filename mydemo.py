7ef25c5e965bce3b085e198d364bd667832063bf
Shania <zixuan_guo@brown.edu--global user.name zguo47>
2024-06-04 23:09:26 -0400

    new

---
+++ mydemo.py
@@ -0,0 +1,70 @@
+from dust3r.inference import inference
+from dust3r.model import AsymmetricCroCo3DStereo
+from dust3r.utils.image import load_images
+from dust3r.image_pairs import make_pairs
+from dust3r.cloud_opt import global_aligner, GlobalAlignerMode
+
+import json
+
+if __name__ == "__main__":
+    device = "cuda"
+    batch_size = 1
+    schedule = "cosine"
+    lr = 0.01
+    niter = 300
+
+    model_name = "naver/DUSt3R_ViTLarge_BaseDecoder_512_dpt"
+    # you can put the path to a local checkpoint in model_name if needed
+    model = AsymmetricCroCo3DStereo.from_pretrained(model_name).to(device)
+    # load_images can take a list of images or a directory
+    images = load_images(
+        "/nfshomes/zguo47/videosfm/4dgaussians/data/skateboard/skateboards", size=512
+    )
+    pairs = make_pairs(images, scene_graph="complete", prefilter=None, symmetrize=True)
+    output = inference(pairs, model, device, batch_size=batch_size)
+
+    # at this stage, you have the raw dust3r predictions
+    view1, pred1 = output["view1"], output["pred1"]
+    view2, pred2 = output["view2"], output["pred2"]
+    # here, view1, pred1, view2, pred2 are dicts of lists of len(2)
+    #  -> because we symmetrize we have (im1, im2) and (im2, im1) pairs
+    # in each view you have:
+    # an integer image identifier: view1['idx'] and view2['idx']
+    # the img: view1['img'] and view2['img']
+    # the image shape: view1['true_shape'] and view2['true_shape']
+    # an instance string output by the dataloader: view1['instance'] and view2['instance']
+    # pred1 and pred2 contains the confidence values: pred1['conf'] and pred2['conf']
+    # pred1 contains 3D points for view1['img'] in view1['img'] space: pred1['pts3d']
+    # pred2 contains 3D points for view2['img'] in view1['img'] space: pred2['pts3d_in_other_view']
+
+    # next we'll use the global_aligner to align the predictions
+    # depending on your task, you may be fine with the raw output and not need it
+    # with only two input images, you could use GlobalAlignerMode.PairViewer: it would just convert the output
+    # if using GlobalAlignerMode.PairViewer, no need to run compute_global_alignment
+    scene = global_aligner(
+        output, device=device, mode=GlobalAlignerMode.PointCloudOptimizer
+    )
+    loss = scene.compute_global_alignment(
+        init="mst", niter=niter, schedule=schedule, lr=lr
+    )
+
+    # retrieve useful values from scene:
+    imgs = scene.imgs
+    focals = scene.get_focals()
+    poses = scene.get_im_poses()
+    pts3d = scene.get_pts3d()
+    confidence_masks = scene.get_masks()
+
+    frames = []
+
+    for i, (pose, file_path, focal) in enumerate(zip(poses, imgs, focals)):
+        frame_data = {"file_path": file_path, "pose": pose, "focal": focal}
+        frames.append(frame_data)
+
+    data = {"frames": frames}
+
+    with open("poses.json", "w") as f:
+        json.dump(data, f, indent=4)  # Use indent for pretty-printing
+
+    # visualize reconstruction
+    # scene.show()
